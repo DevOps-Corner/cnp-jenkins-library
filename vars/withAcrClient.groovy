713d1aa710e7f18416f24c1cbb0aa337b8b1d339
Kremi Nenkova <kremi.nenkova@gmail.com>
2021-03-30 13:06:06 +0100

    Update aks client version

--- vars/withAcrClient.groovy
+++ vars/withAcrClient.groovy
@@ -1,5 +1,5 @@
 def call(String subscription, Closure block) {
-  withDocker('hmcts/cnp-aks-client:az-2.3.0-kubectl-1.18.0-helm-3.1.2', null) {
+  withDocker('hmcts/cnp-aks-client:az-2.21.0-hotfix1-kubectl-1.20.5-helm-3.5.3', null)
     withSubscriptionLogin(subscription) {
       withRegistrySecrets {
         block.call()
