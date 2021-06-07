835bb255d2b5ec9c79c88e6420befa3f3949790c
Thomas Thornton <>
2021-06-07 15:54:18 +0100

    debug test

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -161,7 +161,7 @@ class Acr extends Az {
     def tagFound = false
     try {
       def tags = this.az "acr repository show-tags -n ${registryName} --subscription ${registrySubscription} --repository ${repository}"
-      //steps.echo "Current tags: ${tags}. Is ${tag} available? ... ${tagFound}"
+      steps.echo "Current tags: ${tags}. Is ${tag} available? ... ${tagFound}"
       tagFound = tags.contains(tag)
     } catch (noTagsError) {
     } // Do nothing -> return false
