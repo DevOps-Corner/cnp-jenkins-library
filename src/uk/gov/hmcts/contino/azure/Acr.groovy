48fa82a12eebda8ca7ed04713629ef439d4f7f9a
Thomas Thornton <>
2021-06-09 11:28:04 +0100

    Original

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -160,10 +160,9 @@ class Acr extends Az {
 
     def tagFound = false
     try {
-      String tags = this.az "acr repository show-tags -n ${registryName} --subscription ${registrySubscription} --repository ${repository} | grep ${tag}"
-      String tagvalue = tags.replaceAll ("\"", "\\\\\"")
-      tagFound = tagvalue.equals(tag)
-      steps.echo "Current tags: ${tagvalue}. Is ${tag} available? ... ${tagFound}"
+      def tags = this.az "acr repository show-tags -n ${registryName} --subscription ${registrySubscription} --repository ${repository}"
+      tagFound = tags.contains(tag)
+      steps.echo "Current tags: ${tags}. Is ${tag} available? ... ${tagFound}"
     } catch (noTagsError) {
     } // Do nothing -> return false
 
